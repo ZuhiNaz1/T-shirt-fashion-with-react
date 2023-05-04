e8c618840a19b7fa0f61699d9df7e3b400337dc6
ZuhiNaz1 <zuhinaz1518@gmail.com>
2023-05-04 10:53:09 +0100

    second commit

--- src/components/TShirt/TShirt.js
+++ src/components/TShirt/TShirt.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import './TShirt.css';
 
 const TShirt = ({tshirt}) => {
-    const {picture, name} = tshirt;
+    const {picture, name, price} = tshirt;
     return (
-        <div>
+        <div className='t-shirt'>
             <img src={picture}></img>
+            <h2>{name}</h2>
+            <h4>Price: {price}</h4>
+            <button>Buy this</button>
+
         </div>
     );
 };
